b3526e326a4f89b21bdf238d64104d615da28e38
laco and maco <lacomaco122@gmail.com>
2024-01-20 22:31:51 +0900

    초기 커밋.

---
+++ GameEngine/SDL/README.txt
@@ -0,0 +1,5 @@
+
+1. SDL-devel-2.28.5-VC
+2. SDL2_image-devel.2.8.2-VC
+3. SDL2_ttf_devel.2.20.2-VC
+4. SDL2-mixer-devel.2.7.2-VC
---
+++ GameEngine/src/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <SDL.h>
+#include <glm/glm.hpp>
+#include <sol/sol.hpp>
+
+int main(int argc, char* argv[]) {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    glm::vec2 velocity = glm::vec2(2.0, -1.0);
+    SDL_Init(SDL_INIT_EVERYTHING);
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
