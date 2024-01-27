819134395cc5dbf4e6168f0afd755e1feaf0a555
laco and maco <lacomaco122@gmail.com>
2024-01-28 02:14:33 +0900

    QuadTree 자료구조/알고리즘 추가
    코드가 어떻게 변경될지 몰라서
    
    QuadTree 사용시 Rectangle은 QuadTreeRectangle로 변환하여 넣어주도록 우선 추가함.
    
    지금 컴포넌트는
    
    Move/Transform으로 나눠져있는데, 합치기는 조금 애매해서 그대로 냅둠.
    
    사용시 MoveTransform에서 데이터를 빼내서
    
    QuadTreeRectangle로 entityId와 함께 넘겨주고
    
    충돌 처리는 retrieve를 사용해서 하는게 좋을듯.

---
+++ GameEngine/QuadTree.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <vector>
+#include <memory>
+
+struct QuadTreeRectangle {
+	int x;
+	int y;
+	int width;
+	int height;
+	int entityId;
+
+	QuadTreeRectangle(int x = 0, int y = 0, int width = 0, int height = 0, int entityId = 0) {
+		this->x = x;
+		this->y = y;
+		this->width = width;
+		this->height = height;
+		this->entityId = entityId;
+	}
+};
+
+class QuadTree
+{
+private:
+	const int MAX_OBJECTS = 10;
+	const int MAX_LEVELS = 7;
+	QuadTreeRectangle bounds;
+	/*
+	* 0: top right
+	* 1: top left
+	* 2: bottom left
+	* 3: bottom right
+	* 	*/
+	std::vector<std::unique_ptr<QuadTree>> nodes;
+
+	int level;
+	std::vector<QuadTreeRectangle> objects;
+
+public:
+	QuadTree(QuadTreeRectangle pBounds, int pLevel=0);
+	~QuadTree();
+	void Clear();
+	void Split();
+	int GetIndex(QuadTreeRectangle pRect);
+	void Insert(QuadTreeRectangle pRect);
+	std::vector<QuadTreeRectangle>& Retrieve(std::vector<QuadTreeRectangle>& returnObjects, QuadTreeRectangle pRect);
+
+
+};
+
