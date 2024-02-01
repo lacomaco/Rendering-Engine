4505595dc6c246f8a367a43163a512dc9697567b
laco and maco <lacomaco122@gmail.com>
2024-02-02 02:36:09 +0900

    게임파일 다시 복구

--- GameEngine/src/Game/Game.cpp
+++ GameEngine/src/Game/Game.cpp
@@ -3,45 +3,6 @@ Game::Game() {
 	mWindow = nullptr;
 	mIsRunning = true;
 
-	paddleH = 100;
-	thickness = 15;
-
-	paddles.push_back({
-		10,
-		768 / 2.0f - paddleH / 2.0f
-	});
-
-	paddles.push_back({
-		1024 - 10,
-		768 / 2.0f - paddleH / 2.0f
-	});
-
-	mLeftPaddleKeys.push_back(SDL_SCANCODE_W);
-	mLeftPaddleKeys.push_back(SDL_SCANCODE_S);
-	mLeftPaddleKeys.push_back(SDL_SCANCODE_I);
-	mLeftPaddleKeys.push_back(SDL_SCANCODE_K);
-
-	balls.push_back({
-		1024/2.0f,
-		768/2.0f
-	});
-	balls.push_back({
-		1024 / 2.0f,
-		768 / 2.0f - 15.0f
-	});
-	balls.push_back({
-		1024 / 2.0f,
-		768 / 2.0f -40.0f
-	});
-	balls.push_back({
-		1024 / 2.0f,
-		768 / 2.0f - 22.0f
-	});
-	balls.push_back({
-		1024 / 2.0f,
-		768 / 2.0f + 55.0f
-	});
-
 }
 
 bool Game::Initialize() {
@@ -105,19 +66,6 @@ void Game::ProcessInput() {
 	if (state[SDL_SCANCODE_ESCAPE]) {
 		mIsRunning = false;
 	}
-
-	// 패들 이동 감지
-
-	for (int i = 0; i < paddles.size(); i++) {
-		auto& paddle = paddles[i];
-		paddle.mPaddleDir = 0;
-		if (state[mLeftPaddleKeys[i * 2]]) {
-			paddle.mPaddleDir -= 1;
-		}
-		if (state[mLeftPaddleKeys[i * 2 + 1]]) {
-			paddle.mPaddleDir += 1;
-		}
-	}
 }
 
 void Game::UpdateGame() {
@@ -147,73 +95,6 @@ void Game::UpdateGame() {
 	if (deltaTime > 0.05f) {
 		deltaTime = 0.05f;
 	}
-
-	for (auto& paddle : paddles) {
-		auto& mPaddlePos = paddle.mPaddlePos;
-		auto mPaddleDir = paddle.mPaddleDir;
-
-		if (paddle.mPaddleDir != 0) {
-			paddle.mPaddlePos.y += mPaddleDir * 300.0f * deltaTime;
-
-			if (paddle.mPaddlePos.y < thickness) {
-				mPaddlePos.y = thickness;
-			} else if (paddle.mPaddlePos.y > (768.0f - paddleH)) {
-				mPaddlePos.y = 768.0f - paddleH;
-			}
-		}
-	}
-
-	for (auto& ball : balls) {
-		auto& mBallPos = ball.mBallPos;
-		auto& mBallVel = ball.mBallVel;
-
-		mBallPos.x += mBallVel.x * deltaTime;
-		mBallPos.y += mBallVel.y * deltaTime;
-
-		// 공이 벽에 부딪히면 반사
-		if (mBallPos.y <= thickness && mBallVel.y < 0.0f) {
-			mBallVel.y *= -1;
-		}
-		else if (mBallPos.y >= (768 - thickness) && mBallVel.y > 0.0f) {
-			mBallVel.y *= -1;
-		}
-
-		for (auto& paddle : paddles) {
-			auto& mPaddlePos = paddle.mPaddlePos;
-
-			glm::vec2 paddleLeftTop = glm::vec2(
-				mPaddlePos.x,
-				mPaddlePos.y
-			);
-
-			glm::vec2 paddleRightBottom = glm::vec2(
-				mPaddlePos.x + thickness,
-				mPaddlePos.y + paddleH
-			);
-
-			glm::vec2 ballLeftTop = glm::vec2(
-				mBallPos.x,
-				mBallPos.y
-			);
-
-			glm::vec2 ballRightBottom = glm::vec2(
-				mBallPos.x + thickness,
-				mBallPos.y + thickness
-			);
-
-			if (paddleLeftTop.x > ballLeftTop.x &&
-				paddleLeftTop.x < ballRightBottom.x &&
-				paddleLeftTop.y < ballRightBottom.y &&
-				paddleRightBottom.y > ballLeftTop.y
-				) {
-				mBallVel.x *= -1;
-			}
-
-			if (mBallPos.x <= 0) {
-				//mIsRunning = false;
-			}
-		}
-	}
 }
 
 void Game::GenerateOutput() {
@@ -226,51 +107,6 @@ void Game::GenerateOutput() {
 	);
 	SDL_RenderClear(mRenderer);
 
-	SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255);
-	SDL_Rect wall{
-		0,
-		0,
-		1024,
-		thickness
-	};
-
-	SDL_RenderFillRect(mRenderer, &wall);
-
-	SDL_Rect bottomWall{
-		0,
-		768 - thickness,
-		1024,
-		thickness
-	};
-	SDL_RenderFillRect(mRenderer, &bottomWall);
-
-	for (auto& ball : balls) {
-		auto& mBallPos = ball.mBallPos;
-
-		SDL_Rect ball
-		{
-			static_cast<int>(mBallPos.x - thickness / 2),
-			static_cast<int>(mBallPos.y - thickness / 2),
-			thickness,
-			thickness
-		};
-
-		SDL_RenderFillRect(mRenderer, &ball);
-	}
-
-	for (auto& paddle : paddles) {
-		auto& mPaddlePos = paddle.mPaddlePos;
-
-		SDL_Rect paddle
-		{
-			static_cast<int>(mPaddlePos.x - thickness / 2),
-			static_cast<int>(mPaddlePos.y - thickness / 2),
-			thickness,
-			paddleH
-		};
-
-		SDL_RenderFillRect(mRenderer, &paddle);
-	}
 
 	SDL_RenderPresent(mRenderer);
 }
--- GameEngine/src/Game/Game.h
+++ GameEngine/src/Game/Game.h
@@ -4,36 +4,6 @@
 #include <vector>
 #include <cstdlib>
 
-struct Paddle {
-	glm::vec2 mPaddlePos;
-	int mPaddleDir;
-
-	Paddle(float x,float y) {
-		mPaddlePos = glm::vec2(x,y);
-		mPaddleDir = 0;
-	}
-};
-
-struct Ball {
-	glm::vec2 mBallPos;
-	glm::vec2 mBallVel;
-
-	Ball(float x, float y) {
-		int random_numberX = rand() % 100 + 100;
-
-		int random_numberY = rand() % 100 + 100;
-
-		int xflip = rand() % 5;
-		int yflip = rand() % 5;
-
-		xflip = xflip < 2 ? -1 : 1;
-		yflip = yflip < 2 ? -1 : 1;
-
-		mBallPos = glm::vec2(x, y);
-		mBallVel = glm::vec2(random_numberX * xflip, random_numberY * yflip);
-	}
-};
-
 class Game
 {
 public:
@@ -52,12 +22,7 @@ private:
 	bool mIsRunning;
 	Uint32 mTicksCount;
 
-	std::vector<Paddle> paddles;
 	std::vector<SDL_Scancode> mLeftPaddleKeys;
-	std::vector<Ball> balls;
-
-	int paddleH;
-	int thickness;
 
 };
 
