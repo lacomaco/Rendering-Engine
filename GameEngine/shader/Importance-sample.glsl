4a3747715b8476ca87d19863a9b66f7d9cb88965
lacomaco122@gmail.com <lacomaco122@gmail.com>
2024-07-23 15:20:58 +0900

    G 함수 추가

--- GameEngine/shader/Importance-sample.glsl
+++ GameEngine/shader/Importance-sample.glsl
@@ -43,3 +43,24 @@ vec3 ImportanceSampleGGX(vec2 Xi,vec3 N,float roughness){
 
     return normalize(sampleVec);
 }
+
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float a = roughness;
+    float k = (a * a) / 2.0;
+
+    float nom   = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+
+    return nom / denom;
+}
+
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+
+    return ggx1 * ggx2;
+}  
