7eafb895ec00173e9b8c323d71cd2146761f2f6c
lacomaco122@gmail.com <lacomaco122@gmail.com>
2024-08-10 21:20:28 +0900

    르장드르 K 노말 상수, Spherical Harmonics 계산 함수 추가
    Spherical Harmonics 논문 참고하였음
    
    https://3dvar.com/Green2003Spherical.pdf

--- GameEngine/shader/SH-irradiance-compute.glsl
+++ GameEngine/shader/SH-irradiance-compute.glsl
@@ -5,6 +5,62 @@
 
 layout(local_size_x = ThreadGroupX, local_size_y = ThreadGroupY, local_size_z = 1) in;
 
+// see : https://3dvar.com/Green2003Spherical.pdf
+double P(int l,int m,double x)
+{
+	double pmm = 1.0;
+
+	if(m>0) {
+		double somx2 = sqrt((1.0-x)*(1.0+x));
+		double fact = 1.0;
+		for(int i=1; i<=m; i++) {
+			pmm *= (-fact) * somx2;
+			fact += 2.0;
+		}
+	}
+
+	if(l==m) {
+		return pmm;
+	}
+
+	double pmmp1 = x * (2.0*m+1.0) * pmm;
+
+	if(l==m+1) {
+		return pmmp1;
+	}
+
+	double pll = 0.0;
+
+	for(int ll=m+2; ll<=l; ++ll) {
+		pll = ((2.0*ll-1.0) * x * pmmp1-(ll+m-1.0)*pmm) / (ll-m);
+		pmm = pmmp1;
+		pmmp1 = pll;
+	}
+
+	return pll;
+}
+
+double K(int l, int m)
+{
+	double temp = ((2.0*l+1.0)*factorial(l-m)) / (4.0*PI*factorial(l+m));
+	return sqrt(temp);
+}
+
+double SH(int l, int m, double theta, double phi)
+{
+	const double sqrt2 = sqrt(2.0);
+
+	if(m==0) {
+		return K(l,0)*P(l,m,cos(theta));
+	}
+	else if(m>0) {
+		return sqrt2*K(l,m)*cos(m*phi)*P(l,m,cos(theta));
+	}
+	else {
+		return sqrt2*K(l,-m)*sin(-m*phi)*P(l,-m,cos(theta));
+	}
+}
+
 void main () {
 
 }
